1b28a501e192fbb2b90909ffc25060bf3b59d422
ayelenst <ayelenst@gmail.com>
2017-07-06 11:56:02 -0300

    Revert "removing dashboard to re create"
    This reverts commit 2a0fff365859167014798de8ac984cc9c4813c42.

---
+++ ecommerce/Dashboard/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Dashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dashboard.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            var app = new ServiceReference.ContractClient();
+            var categories = app.GetAllCategories();
+
+            var model = new List<Category>();
+            foreach (var c in categories)
+            {
+                var cat = new Category
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                };
+                model.Add(cat);
+            }
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
